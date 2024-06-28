2012cc7363924839ee6816709249620ce5e1ebfe
Nephys <nephys.nextsoulstudio@gmail.com>
2024-06-28 22:30:49 +0200

    Added TODO/FIXME highlighting

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -9,3 +9,14 @@ autocmd('TextYankPost', {
     vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '125' })
   end
 })
+
+-- TODO, FIXME... highlighter
+augroup('todo', { clear = true })
+vim.api.nvim_set_hl(0, 'TodoHighlight', { ctermfg = 'Magenta', fg = '#CBA6F7' }) -- Highlight color
+autocmd('Syntax', {
+    group = 'todo',
+    pattern = '*',
+    callback = function()
+        vim.fn.matchadd('TodoHighlight', [[\v\W\zs<(NOTE|INFO|IDEA|TODO|FIXME|CHANGED|BUG|HACK|TRICKY|DEPRECATED)>]])
+    end
+})
